e8d86739a152ded0606cccb1f79204c1e705ecd7
Jiamin WANG <wjm.imtld@gmail.com>
2018-06-04 23:41:18 +0200

    commit apres merge

--- edu.telecom.lille.projet.info.etudiantTimeTable/src/timeTableModel/Room.java
+++ edu.telecom.lille.projet.info.etudiantTimeTable/src/timeTableModel/Room.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*******************************************************************************
  * 2018, All rights reserved.
  *******************************************************************************/
@@ -83,81 +83,3 @@ public class Room {
 	}
 
 }
-=======
-/*******************************************************************************
- * 2018, All rights reserved.
- *******************************************************************************/
-package timeTableModel;
-
-// Start of user code (user defined imports)
-
-// End of user code
-
-/**
- * Description of Room.
- * 
- * @author Jiamin WANG
- */
-public class Room {
-	/**
-	 * Description of the property RoomId.
-	 */
-	private int RoomId = 0;
-
-	/**
-	 * Description of the property Capacity.
-	 */
-	private int Capacity = 0;
-
-	// Start of user code (user defined attributes for Room)
-
-	// End of user code
-
-	/**
-	 * The constructor.
-	 */
-	public Room(int _roomId,int _capacity) {
-		// Start of user code constructor for Room)
-        this.RoomId = _roomId;
-        this.Capacity = _capacity;
-		super();
-		// End of user code
-	}
-
-	// Start of user code (user defined methods for Room)
-
-	// End of user code
-	/**
-	 * Returns RoomId.
-	 * @return RoomId 
-	 */
-	public int getRoomId() {
-		return this.RoomId;
-	}
-
-	/**
-	 * Sets a value to attribute RoomId. 
-	 * @param newRoomId 
-	 */
-	public void setRoomId(int newRoomId) {
-		this.RoomId = newRoomId;
-	}
-
-	/**
-	 * Returns Capacity.
-	 * @return Capacity 
-	 */
-	public int getCapacity() {
-		return this.Capacity;
-	}
-
-	/**
-	 * Sets a value to attribute Capacity. 
-	 * @param newCapacity 
-	 */
-	public void setCapacity(int newCapacity) {
-		this.Capacity = newCapacity;
-	}
-
-}
->>>>>>> branch 'master' of git@github.com:Jiamin-WANG/ProjetInfo.git
